99579d3446e764b6f4a09500c1df07ae105910ae
Cynthia Mugo <cynthiamugo25@gmail.com>
2025-08-08 09:10:47 +0300

    word ladder shortest transformation sequence

---
+++ Python/word-ladder.py
@@ -0,0 +1,18 @@
+# Given two words, beginWord and endWord, and a dictionary of words wordList, return the number of words in the shortest transformation sequence from beginWord to endWord, such that:
+
+# Only one letter can be changed at a time.
+# Each transformed word must exist in the word list.
+# Return 0 if no such sequence exists.
+# All words have the same length and consist of lowercase letters only.
+
+# beginWord is not considered a transformed word and may not be in wordList.
+# Example:
+# Input:
+# beginWord = "hit"
+# endWord = "cog"
+# wordList = ["hot","dot","dog","lot","log","cog"]
+
+# Output: 5
+
+def word_ladder():
+    pass
