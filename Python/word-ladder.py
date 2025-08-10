57dc9ee8f62b1f8f1e09d649efb7a04dad1899fe
Cynthia Mugo <cynthiamugo25@gmail.com>
2025-08-10 18:36:07 +0300

    word ladder

--- Python/word-ladder.py
+++ Python/word-ladder.py
@@ -14,5 +14,25 @@
 
 # Output: 5
 
-def word_ladder():
-    pass
+def word_ladder(beginWord, endWord, wordList):
+    wordSet = set(wordList)
+    if endWord not in wordSet:
+        return 0
+    
+    queue = [(beginWord, 1)]
+    
+    while queue:
+        word, steps = queue.pop(0)
+        
+        if word == endWord:
+            return steps
+        
+        for i in range(len(word)):
+            for c in 'abcdefghijklmnopqrstuvwxyz':
+                new_word = word[:i] + c + word[i+1:]
+                if new_word in wordSet:
+                    wordSet.remove(new_word)
+                    queue.append((new_word, steps + 1))
+    
+    return 0
+print(word_ladder("hit", "cog", ["hot","dot","dog","lot","log","cog"]))
